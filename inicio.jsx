ee4b80ef1b5acb3f50fa71680441e4fe772fa9f3
Valentina <valen.arbarello@gmail.com>
2022-06-10 18:11:49 -0300

    Co-authored-by: SRUR IARA <46026326@comunidadort.edu.ar>

--- inicio.jsx
+++
@@ -1,3 +0,0 @@
-<h1>hola</h1>
-<p> hoy es domigo </p>
-<h1>que tal</h1> 
---
+++ src/components/prubea.js
@@ -0,0 +1,16 @@
+export default function Prueba(){
+    return (
+    <div className = "prueba">
+        <iframe 
+        title= "mapa"
+        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d74351.23101126155!2d-58.43204101120412!3d-34.55630933594325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sar!4v1654895128266!5m2!1ses-419!2sar" 
+        width="600" 
+        height="450" 
+        style={{border:0}}
+        allowfullscreen="" 
+        loading="lazy" 
+        referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+         </div>    
+    );
+}
